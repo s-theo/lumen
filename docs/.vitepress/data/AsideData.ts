bfa859b384aca9fe98e8446e1cf01c307ae8e194
Theo <s.theojs@gmail.com>
2025-01-02 15:29:40 +0800

    feat(AsideLogo): use Iconify icons and support light/dark modes. (#194)

--- docs/.vitepress/data/AsideData.ts
+++ docs/.vitepress/data/AsideData.ts
@@ -3,21 +3,21 @@ import type { AsideItem } from '@theojs/lumen'
 export const Aside_Data: AsideItem[] = [
   {
     link: 'https://qytcc01.qingyunti.pro/register?aff=jjgD79Jd',
-    icon: 'https://www.qingyunti.cc/images/qytlogo-144-49.png',
+    image: 'https://www.qingyunti.cc/images/qytlogo-144-49.png',
     activity: '青云梯 四周年双旦特惠',
     info1: '月付 85折 优惠码 <span style="color: var(--vp-c-brand-3)"> qyt85 </span>',
     info2: '年付以上 8折 优惠码 <span style="color: var(--vp-c-brand-3)"> qyt80 </span>'
   },
   {
     link: 'https://fbinv02.fbaff.cc/auth/register?code=RZP3',
-    icon: 'https://i.theojs.cn/docs/202409111237242.png',
+    image: 'https://i.theojs.cn/docs/202409111237242.png',
     activity: 'FlyingBird 双旦活动开启',
     info1: '月付 85折 优惠码 <span style="color: var(--vp-c-brand-3)"> 122485 </span>',
     info2: '年付 64折 优惠码 <span style="color: var(--vp-c-brand-3)"> 122480 </span>'
   },
   // {
   //   link: 'https://feitu.im/index.html?register=2cFF8mg4',
-  //   icon: 'https://飞兔.com/img/logo.png',
+  //   image: 'https://飞兔.com/img/logo.png',
   //   activity: '飞兔云 双11 狂欢来袭！',
   //   info1:
   //     '全场<span style="color: var(--vp-c-brand-3)"> 88 </span>折！全场<span style="color: var(--vp-c-brand-3)"> 88 </span>折！',
@@ -25,33 +25,34 @@ export const Aside_Data: AsideItem[] = [
   // },
   {
     link: 'https://github.com/sponsors/Theo-Messi',
-    icon: 'https://doc.theojs.cn/爱心.png',
+    icon: 'twemoji:red-heart',
     name: '赞助我的项目'
   },
   {
     link: 'https://doc.theojs.cn/',
-    icon: 'https://i.theojs.cn/avatar.png',
+    image: 'https://i.theojs.cn/avatar.png',
     name: 'Theo-Docs',
     hide1: '流媒体观影',
     hide2: '一站式服务'
   },
   {
     link: 'https://xx.theojs.cn/',
-    icon: 'https://i.theojs.cn/avatar.png',
+    image: 'https://i.theojs.cn/avatar.png',
     name: '玄学宝典',
     hide1: '传世经典著作',
     hide2: '山医命相卜'
   },
   {
     link: 'https://doc.theojs.cn/serve/sharing/Account-sharing-guide',
-    icon: 'https://cdn.iconscout.com/icon/free/png-256/free-netflix-2296042-1912001.png',
+    icon: { light: 'simple-icons:netflix', dark: 'simple-icons:youtube' },
+    color: { light: '#E50914', dark: '#FF0000' },
     name: '流媒体帐号合租',
     hide1: '流媒体账号合租',
     hide2: '共享车位'
   },
   {
     link: 'https://github.com/Theo-Messi/dotfiles',
-    icon: 'https://img.icons8.com/?size=400&id=nuPce-GYYZeC&format=png&color=000000',
+    icon: 'eos-icons:configuration-file',
     name: '常用配置 - dotfiles',
     hide1: 'zshrc、系统优化一键脚本',
     hide2: 'Clash/Loon/QX... 配置'
