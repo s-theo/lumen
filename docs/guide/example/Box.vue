7e5f7ed841717ada47963e7bb3ed81f34bf21df3
Theo <fanxiaobin422@gmail.com>
2025-01-06 15:51:28 +0800

    docs: update example.

---
+++ docs/guide/example/Box.vue
@@ -0,0 +1,40 @@
+<Box
+  :items="[
+    // iconify图标
+    {
+      name: 'iconify',
+      link: '#',
+      icon: 'line-md:iconify2-static',
+      color: '#1769AA'
+    },
+    //深浅模式的iconify图标
+    {
+      name: 'Vercel',
+      link: '#',
+      icon: { light: 'ion:logo-vercel', dark: 'ion:logo-vercel' },
+      color: { light: '#000000', dark: '#FFFFFF' }
+    },
+    {
+      name: 'Vite',
+      link: '#',
+      icon: { light: 'skill-icons:vite-light', dark: 'skill-icons:vite-dark' }
+    },
+    //图片
+    {
+      name: '支付宝',
+      link: 'https://i.theojs.cn/docs/202405201752089.jpg',
+      image: 'https://i.theojs.cn/logo/alipay.svg'
+    },
+    //深浅模式的图片
+    {
+      name: 'GitHub',
+      link: '#',
+      image: {
+        light: 'https://i.theojs.cn/logo/github.svg',
+        dark: 'https://i.theojs.cn/logo/github-dark.svg'
+      }
+    },
+    //标签
+    { name: 'Vue', link: '#', icon: 'vscode-icons:file-type-vue', tag: 'vuejs' }
+  ]"
+/>
---
+++ docs/guide/example/Links.vue
@@ -0,0 +1,51 @@
+<Links
+  :items="[
+    // iconify图标
+    {
+      name: 'iconify',
+      link: '#',
+      icon: 'line-md:iconify2-static',
+      color: '#1769AA'
+    },
+    //深浅模式的iconify图标
+    {
+      name: 'Vercel',
+      link: '#',
+      icon: { light: 'ion:logo-vercel', dark: 'ion:logo-vercel' },
+      color: { light: '#000000', dark: '#FFFFFF' }
+    },
+    {
+      name: 'Vite',
+      link: '#',
+      icon: { light: 'skill-icons:vite-dark', dark: 'skill-icons:vite-light' }
+    },
+    //图片
+    {
+      name: '支付宝',
+      link: 'https://i.theojs.cn/docs/202405201752089.jpg',
+      image: 'https://i.theojs.cn/logo/alipay.svg'
+    },
+    //深浅模式的图片
+    {
+      name: 'GitHub',
+      link: '#',
+      image: {
+        light: 'https://i.theojs.cn/logo/github.svg',
+        dark: 'https://i.theojs.cn/logo/github-dark.svg'
+      }
+    },
+    // 使用图标 + 描述
+    {
+      name: 'VitePress',
+      link: 'https://www.google.com',
+      image: 'https://vitepress.dev/vitepress-logo-large.webp',
+      desc: 'VitePress 是一个由 Vue 团队开发的静态网站生成器，基于 Vite 和 Vue 3，专注于文档、博客和轻量网站的构建。它支持 Markdown 和 Vue 组件，提供简洁的默认主题和灵活的自定义能力，适合快速创建高性能的技术文档和个人博客。'
+    },
+    // 不使用图标 + 描述
+    {
+      name: 'Google',
+      link: 'https://www.google.com',
+      desc: '全球最大的搜索引擎'
+    }
+  ]"
+/>
