e45374f99cd340d5d65b8874bc4d152a969309f7
Theo <fanxiaobin422@gmail.com>
2025-01-20 16:10:35 +0800

    feat: use em as the relative unit and add variables in "components-var". (#225)

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -6,15 +6,15 @@ export const nav: DefaultTheme.NavItem[] = [
     link: '/guide/getting-started'
   },
   {
-    text: '<iconify-icon icon="fa6-solid:gears" style="margin-right:0.25rem;color:#3498db;"></iconify-icon>配置',
+    text: '<iconify-icon icon="fa6-solid:gears" style="margin-right:0.25em;color:#3498db;"></iconify-icon>配置',
     link: '/guide/theme'
   },
   {
-    text: '<iconify-icon icon="fa6-solid:bug" style="margin-right:0.25rem;color:#FF4500;" alt="bug"></iconify-icon>提交反馈',
+    text: '<iconify-icon icon="fa6-solid:bug" style="margin-right:0.25em;color:#FF4500;" alt="bug"></iconify-icon>提交反馈',
     link: 'https://github.com/Theo-Messi/lumen/issues'
   },
   {
-    text: '<iconify-icon icon="fa6-solid:clipboard-list" style="margin-right:0.25rem;color:#FF6347;" alt="clipboard"></iconify-icon>变更日志',
+    text: '<iconify-icon icon="fa6-solid:clipboard-list" style="margin-right:0.25em;color:#FF6347;" alt="clipboard"></iconify-icon>变更日志',
     link: `https://github.com/Theo-Messi/lumen/releases`
   }
 ]
