d5289c34eb4519a3d7c70e49082d51e2ede40d4d
Theo <s.theojs@gmail.com>
2025-05-17 15:21:25 +0800

    chore: update `520&618` campaign message

--- docs/.vitepress/data/AsideData.ts
+++ docs/.vitepress/data/AsideData.ts
@@ -2,6 +2,22 @@ import type { AsideItem } from '@theojs/lumen'
 
 export const Aside_Data: AsideItem[] = [
   {
+    link: 'https://itheo.top/flyingbird',
+    image: 'https://i.theojs.cn/logo/flyingbird.webp',
+    alt: 'FlyingBird Logo',
+    promo: 'FlyingBird 520&618 特惠',
+    info1:
+      '月付 <span class="promo-text"> 85折 </span>优惠码 <span class="promo-text"> fb2561885 </span>',
+    info2:
+      '年付 <span class="promo-text"> 64折 </span>优惠码 <span class="promo-text"> fb2561880 </span>'
+  },
+  // {
+  //   link: 'https://itheo.top/flyingbird',
+  //   image: 'https://i.theojs.cn/logo/flyingbird.webp',
+  //  alt: 'FlyingBird Logo'
+  //   promo: 'FlyingBird'
+  // },
+  {
     link: 'https://itheo.top/totoro',
     image: 'https://i.theojs.cn/logo/totoro.webp',
     alt: '龙猫云 Logo',
@@ -11,40 +27,42 @@ export const Aside_Data: AsideItem[] = [
     info2:
       '年付 <span class="promo-text"> 8折 </span>优惠码 <span class="promo-text"> spring80 </span>'
   },
+  // {
+  //   link: 'https://itheo.top/totoro',
+  //   image: 'https://i.theojs.cn/logo/totoro.webp',
+  //   alt: '龙猫云 Logo',
+  //   promo: 'Totoro Cloud - 龙猫云'
+  // },
+  // {
+  //   link: 'https://itheo.top/qyt',
+  //   image: 'https://i.theojs.cn/logo/qyt.webp',
+  //   alt: '青云梯 Logo',
+  //   promo: '青云梯 51劳动节活动开启',
+  //   info1:
+  //     '月付 <span class="promo-text"> 85折 </span>优惠码 <span class="promo-text"> wuyi85 </span>',
+  //   info2:
+  //     '年付 <span class="promo-text"> 8折 </span>优惠码 <span class="promo-text"> wuyi80 </span>'
+  // },
   {
     link: 'https://itheo.top/qyt',
     image: 'https://i.theojs.cn/logo/qyt.webp',
     alt: '青云梯 Logo',
-    promo: '青云梯 51劳动节活动开启',
-    info1:
-      '月付 <span class="promo-text"> 85折 </span>优惠码 <span class="promo-text"> wuyi85 </span>',
-    info2:
-      '年付 <span class="promo-text"> 8折 </span>优惠码 <span class="promo-text"> wuyi80 </span>'
+    promo: '青云梯'
   },
+  // {
+  //   link: 'https://itheo.top/tnt',
+  //   image: 'https://i.theojs.cn/docs/TNT.webp',
+  //   alt: 'TNT Cloud Logo',
+  //   promo: 'TNT Cloud 五一特惠!',
+  //   info1:
+  //     '全场 <span class="promo-text"> 8折 </span>优惠码 <span class="promo-text"> MayDay80 </span>'
+  // },
   {
     link: 'https://itheo.top/tnt',
     image: 'https://i.theojs.cn/docs/TNT.webp',
     alt: 'TNT Cloud Logo',
-    promo: 'TNT Cloud 五一特惠!',
-    info1:
-      '全场 <span class="promo-text"> 8折 </span>优惠码 <span class="promo-text"> MayDay80 </span>'
+    promo: 'TNT Cloud'
   },
-  {
-    link: 'https://itheo.top/flyingbird',
-    image: 'https://i.theojs.cn/logo/flyingbird.webp',
-    alt: 'FlyingBird Logo',
-    promo: 'FlyingBird 五一活动开启咯',
-    info1:
-      '月付 <span class="promo-text"> 85折 </span>优惠码 <span class="promo-text"> fb255185 </span>',
-    info2:
-      '年付 <span class="promo-text"> 64折 </span>优惠码 <span class="promo-text"> fb255180 </span>'
-  },
-  // {
-  //   link: 'https://itheo.top/totoro',
-  //   image: 'https://i.theojs.cn/logo/totoro.webp',
-  // alt: '龙猫云 Logo',
-  //   promo: 'Totoro Cloud - 龙猫云'
-  // },
   // {
   //   link: 'https://itheo.top/feitu',
   //   image: 'https://i.theojs.cn/logo/feitu.webp',
@@ -53,24 +71,6 @@ export const Aside_Data: AsideItem[] = [
   //   info1:
   //     '全场 <span class="promo-text"> 88 </span>折！全场 <span class="promo-text"> 88 </span>折！',
   //   info2: '优惠码 <span class="promo-text"> 1111@88 </span>全场打折'
-  // },
-  // {
-  //   link: 'https://itheo.top/qyt',
-  //   image: 'https://i.theojs.cn/logo/qyt.webp',
-  //alt: '青云梯 Logo',
-  //   promo: '青云梯'
-  // },
-  // {
-  //   link: 'https://itheo.top/tnt',
-  //   image: 'https://i.theojs.cn/docs/TNT.webp',
-  // alt: 'TNT Cloud Logo',
-  //   promo: 'TNT Cloud'
-  // },
-  // {
-  //   link: 'https://itheo.top/flyingbird',
-  //   image: 'https://i.theojs.cn/logo/flyingbird.webp',
-  //  alt: 'FlyingBird Logo'
-  //   promo: 'FlyingBird'
   // },
   {
     link: 'https://itheo.top/feitu',
