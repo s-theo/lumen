8cbe28b1576a2174d3c56a96525aac9fec3ca402
Theo <fanxiaobin422@gmail.com>
2025-05-26 13:34:50 +0800

    refactor(Link): extract IconDisplay ImageDisplay Link (#276)

---
+++ src/composables/Link.vue
@@ -0,0 +1,21 @@
+您说：
+<script setup lang="ts">
+import { isExternal, LinkType, RelType, ClassesType } from '../types'
+
+const props = defineProps<{
+  href: LinkType
+  rel?: RelType
+  classes?: ClassesType
+}>()
+</script>
+
+<template>
+  <a
+    :class="props.classes"
+    :href="props.href"
+    :target="isExternal(props.href) ? '_blank' : '_self'"
+    :rel="props.rel ?? (isExternal(props.href) ? 'noreferrer' : undefined)"
+  >
+    <slot />
+  </a>
+</template>
---
+++ src/composables/index.ts
@@ -0,0 +1,3 @@
+export { default as IconDisplay } from './IconDisplay.vue'
+export { default as ImageDisplay } from './ImageDisplay.vue'
+export { default as Link } from './Link.vue'
