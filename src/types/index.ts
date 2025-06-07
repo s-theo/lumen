bff947ca121e4aa537fd3a1cd794becb87449975
Theo <s.theojs@gmail.com>
2025-06-07 16:16:11 +0800

    fix(waline): async load Waline to fix recaptcha issue

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import { defineAsyncComponent } from 'vue'
+
 import '../theme/components-var.css'
 
 import 'iconify-icon'
@@ -8,10 +10,13 @@ export { default as DocBox } from '../components/DocBox.vue'
 export { default as DocPill } from '../components/DocPill.vue'
 export { default as DocBoxCube } from '../components/DocBoxCube.vue'
 export { default as DocLinks } from '../components/DocLinks.vue'
-export { default as Waline } from '../components/DocWaline.vue'
 export { default as DocVideoLink } from '../components/DocVideoLink.vue'
 export { default as HomeFooter } from '../components/HomeFooter.vue'
 export { default as HomeUnderline } from '../components/HomeUnderline.vue'
+
+export const Waline = defineAsyncComponent(
+  () => import('../components/DocWaline.vue')
+)
 
 export { Icon } from '@iconify/vue'
 export * from '../composables'
