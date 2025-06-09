b36a3018e5bd7f4698d2cbc6429a7a9587b6bdff
Theo <fanxiaobin422@gmail.com>
2025-06-09 23:03:00 +0800

    chore: add custom reactions and emoji sets

--- docs/.vitepress/data/Waline.ts
+++ docs/.vitepress/data/Waline.ts
@@ -4,5 +4,31 @@ export const Waline_Data: WalineData = {
   serverURL: 'https://waline-lumen.netlify.app/.netlify/functions/comment/',
   lang: 'zh-CN',
   noCopyright: true,
-  reaction: true
+  reaction: [
+    'https://unpkg.com/@waline/emojis@latest/tw/1f44d.png',
+    'https://unpkg.com/@waline/emojis@latest/tw-emoji/1f602.png',
+    'https://unpkg.com/@waline/emojis@latest/tw-emoji/1f60d.png',
+    'https://unpkg.com/@waline/emojis@latest/tw-emoji/1f609.png',
+    'https://unpkg.com/@waline/emojis@latest/tw-emoji/1f60e.png',
+    'https://unpkg.com/@waline/emojis@latest/tw-emoji/1f612.png'
+  ],
+  locale: {
+    reactionTitle: '已到达文章底部，欢迎留下你的评论与表情~',
+    reaction0: '点赞支持',
+    reaction1: '哈哈大笑',
+    reaction2: '爱死这个了',
+    reaction3: '眨个眼',
+    reaction4: '酷到爆',
+    reaction5: '无语了'
+  },
+  emoji: [
+    'https://unpkg.com/@waline/emojis@latest/alus',
+    'https://unpkg.com/@waline/emojis@latest/bilibili',
+    'https://unpkg.com/@waline/emojis@latest/bmoji',
+    'https://unpkg.com/@waline/emojis@latest/qq',
+    'https://unpkg.com/@waline/emojis@latest/tieba',
+    'https://unpkg.com/@waline/emojis@latest/tw',
+    'https://unpkg.com/@waline/emojis@latest/weibo',
+    'https://unpkg.com/@waline/emojis@latest/soul-emoji'
+  ]
 }
