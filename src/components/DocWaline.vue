40382c0ebf5b8a2bcaff88937c39a841e974b661
Theo <fanxiaobin422@gmail.com>
2025-06-10 12:01:01 +0800

    fix(waline): avoid hydration issues in Waline

--- src/components/DocWaline.vue
+++ src/components/DocWaline.vue
@@ -13,28 +13,30 @@ const { lang, isDark } = useData()
 </script>
 
 <template>
-  <Waline
-    :serverURL="data.serverURL"
-    :path="route.path"
-    :lang="data.lang || lang"
-    :locale="data.locale"
-    :emoji="data.emoji"
-    :dark="isDark"
-    :commentSorting="data.commentSorting"
-    :meta="data.meta"
-    :requiredMeta="data.requiredMeta"
-    :login="data.login"
-    :wordLimit="data.wordLimit"
-    :pageSize="data.pageSize"
-    :imageUploader="data.imageUploader"
-    :highlighter="data.highlighter"
-    :texRenderer="data.texRenderer"
-    :search="data.search"
-    :noCopyright="data.noCopyright"
-    :recaptchaV3Key="data.recaptchaV3Key"
-    :turnstileKey="data.turnstileKey"
-    :reaction="data.reaction"
-  />
+  <ClientOnly>
+    <Waline
+      :serverURL="data.serverURL"
+      :path="route.path"
+      :lang="data.lang || lang"
+      :locale="data.locale"
+      :emoji="data.emoji"
+      :dark="isDark"
+      :commentSorting="data.commentSorting"
+      :meta="data.meta"
+      :requiredMeta="data.requiredMeta"
+      :login="data.login"
+      :wordLimit="data.wordLimit"
+      :pageSize="data.pageSize"
+      :imageUploader="data.imageUploader"
+      :highlighter="data.highlighter"
+      :texRenderer="data.texRenderer"
+      :search="data.search"
+      :noCopyright="data.noCopyright"
+      :recaptchaV3Key="data.recaptchaV3Key"
+      :turnstileKey="data.turnstileKey"
+      :reaction="data.reaction"
+    />
+  </ClientOnly>
 </template>
 
 <style>
