444678bb5383bcc93d9eb3649ee4c5b314ef6173
Theo <s.theojs@gmail.com>
2025-06-12 19:36:48 +0800

    fix(mobile): override grid layout to resolve display issues

---
+++ docs/guide/example/Links-grid-3.vue
@@ -0,0 +1,30 @@
+<Links
+  :grid="3"
+  :items="[
+    // 深浅模式 iconify 图标
+    {
+      name: 'Vercel',
+      link: 'https://vercel.com/',
+      icon: { light: 'ion:logo-vercel', dark: 'ion:logo-vercel' },
+      color: { light: '#000000', dark: '#FFFFFF' },
+      desc: '前端云平台，提供无服务器部署、静态站点生成和实时预览等功能，旨在帮助开发者快速构建和部署现代 Web 应用。',
+      alt: 'Vercel icon',
+      rel: 'noopener noreferrer'
+    },
+    // 深浅模式 iconify 图标，无 color
+    {
+      name: 'Vite',
+      link: 'https://vitejs.dev/',
+      icon: { light: 'skill-icons:vite-light', dark: 'skill-icons:vite-dark' },
+      alt: 'Vite icon',
+      desc: '现代前端构建工具，提供快速的开发服务器和高效的打包功能，支持热模块替换和按需加载，旨在提升前端开发体验。',
+      rel: 'noopener noreferrer'
+    },
+    // 无图标
+    {
+      name: 'Google',
+      link: 'https://www.google.com',
+      rel: 'noopener noreferrer'
+    }
+  ]"
+/>
