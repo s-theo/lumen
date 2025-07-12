5c41d255892a542b8700caf414d2a64e6177529b
Theo <fanxiaobin422@gmail.com>
2025-07-12 22:50:02 +0800

    docs(style): reorder elements to match UI layout order

--- docs/guide/example/FooterData_i18n.ts
+++ docs/guide/example/FooterData_i18n.ts
@@ -6,15 +6,12 @@ export const Footer_Data: FooterData = {
       beian: {
         showIcon: true,
         icp: {
-          number: '粤ICP备12345678号',
           icon: {
             light: 'fluent:globe-shield-48-filled',
             dark: 'fluent:globe-shield-48-filled',
-            color: {
-              light: 'rgba(20, 150, 255, 1)',
-              dark: 'rgba(100, 200, 255, 1)'
-            }
+            color: { light: 'rgba(20, 150, 255, 1)', dark: 'rgba(100, 200, 255, 1)' }
           },
+          number: '粤ICP备12345678号',
           rel: 'noopener noreferrer'
         }
       }
@@ -24,15 +21,12 @@ export const Footer_Data: FooterData = {
       beian: {
         showIcon: true,
         icp: {
-          number: 'ICP 12345678',
           icon: {
             light: 'fluent:globe-shield-48-filled',
             dark: 'fluent:globe-shield-48-filled',
-            color: {
-              light: 'rgba(20, 150, 255, 1)',
-              dark: 'rgba(100, 200, 255, 1)'
-            }
+            color: { light: 'rgba(20, 150, 255, 1)', dark: 'rgba(100, 200, 255, 1)' }
           },
+          number: 'ICP 12345678',
           rel: 'noopener noreferrer'
         }
       }
--- docs/guide/example/Links.vue
+++ docs/guide/example/Links.vue
@@ -1,13 +1,11 @@
 <Links
   :items="[
-    // iconify 图标
     {
-      name: 'iconify',
-      link: 'https://iconify.design/',
       icon: { icon: 'line-md:iconify2-static', color: '#1769AA' },
       size: 48,
+      name: 'iconify',
       desc: '开源的图标库，提供了数以万计的矢量图标，支持多种框架和平台。它允许开发者轻松地在项目中使用各种图标，并且支持深浅模式切换。',
-      rel: 'noopener noreferrer'
+      link: 'https://iconify.design/'
     }
   ]"
 />
