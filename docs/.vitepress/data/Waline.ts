b062b07a632cfdfb6c99f759c05511ed1e02808b
Theo <s.theojs@gmail.com>
2025-07-12 13:39:05 +0800

    chore(waline): use custom domain for Waline server

--- docs/.vitepress/data/Waline.ts
+++ docs/.vitepress/data/Waline.ts
@@ -1,7 +1,7 @@
 import type { WalineData } from '@theojs/lumen'
 
 export const Waline_Data: WalineData = {
-  serverURL: 'https://waline-lumen.netlify.app/.netlify/functions/comment/',
+  serverURL: 'https://waline.theojs.cn/.netlify/functions/comment',
   lang: 'zh-CN',
   noCopyright: true,
   reaction: [
