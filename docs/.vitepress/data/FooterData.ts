5daeb783fee81549337113f83621d6130014c8c0
Theo <fanxiaobin422@gmail.com>
2025-11-19 11:16:55 +0800

    chore: update promo

--- docs/.vitepress/data/FooterData.ts
+++ docs/.vitepress/data/FooterData.ts
@@ -19,13 +19,14 @@ export const Footer_Data: FooterData = {
       title: '优质线路',
       links: [
         { name: '青云梯', link: 'https://itheo.top/qyt', rel: 'sponsored noreferrer' },
+        { name: 'hyperlink - 极连云', link: 'https://itheo.top/hyperlink', rel: 'sponsored noreferrer' },
         { name: 'Galaxy - 银河云', link: 'https://itheo.top/yhy', rel: 'sponsored noreferrer' },
         { name: 'TNT Cloud', link: 'https://itheo.top/tnt', rel: 'sponsored noreferrer' },
         { name: 'FlyingBird - 飞鸟机场', link: 'https://itheo.top/flyingbird', rel: 'sponsored noreferrer' },
         { name: 'Totoro - 龙猫云', link: 'https://itheo.top/totoro', rel: 'sponsored noreferrer' },
         { name: '小蜜蜂', link: 'https://itheo.top/bee', rel: 'sponsored noreferrer' },
         { name: 'OKANC', link: 'https://itheo.top/okanc', rel: 'sponsored noreferrer' },
-        { name: '闪电鼠', link: 'https://itheo.top/sds', rel: 'sponsored noreferrer' }
+        { name: '星岛梦', link: 'https://itheo.top/xdm', rel: 'sponsored noreferrer' }
       ]
     },
     {
