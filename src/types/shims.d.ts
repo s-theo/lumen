dfe65df03936e0cefeddf96b3c41c904c473041f
Theo <fanxiaobin422@gmail.com>
2026-05-07 11:00:28 +0800

    feat(types): improve type definitions and exports

---
+++ src/types/shims.d.ts
@@ -0,0 +1,27 @@
+declare module '*.css' {
+  const content: Record<string, string>
+  export default content
+}
+
+declare module '*.scss' {
+  const content: Record<string, string>
+  export default content
+}
+
+declare module '*.sass' {
+  const content: Record<string, string>
+  export default content
+}
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+declare module 'iconify-icon' {}
+
+declare module '@theojs/lumen/*' {
+  const content: any
+  export default content
+}
